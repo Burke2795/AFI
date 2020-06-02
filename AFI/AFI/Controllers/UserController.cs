1d4c95f919f69627ec936d230180eb85b507a8ec
Sean Burke <burke2795@gmail.com>
2020-06-02 20:48:30 +0100

    Initial project template setup.

---
+++ AFI/AFI/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace AFI.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Create()
+        {
+            return new JsonResult("Success");
+        }
+    }
+}
