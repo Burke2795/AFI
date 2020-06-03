0f4df0a3196dd523e36b88a898d01ef686f7f77f
Sean Burke <burke2795@gmail.com>
2020-06-03 19:39:25 +0100

    Create UserModel. Added custom age validator, email validator via regex.

---
+++ AFI/Business/Models/UserModel.cs
@@ -0,0 +1,20 @@
+﻿using AFI.Validators;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Business.Models
+{
+    public class UserModel
+    {
+        [Required(ErrorMessage = "A forename is needed to create your user."]
+        public string Forename { get; set; }
+        [Required(ErrorMessage = "A surname is needed to create your user.")]
+        public string Surname { get; set; }
+        [RegularExpression(@"^([a-z0-9]{4})@([-a-z0-9]{2})+(\.co.uk|\.com)$", ErrorMessage = "The email address needs to contain 4 alpha numerics followed by @ a minimum of 2 further alphanumberics then either .co.uk or .com eg. 'xxxx@xx.co.uk'")]
+        public string Email { get; set; }
+
+        [AgeValidator(18)]
+        public DateTime? DateofBirth { get; set; }
+        public string ReferenceNumber { get; set; }
+    }
+}
---
+++ AFI/Business/Validators/AgeValidator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AFI.Validators
+{
+    public class AgeValidator : ValidationAttribute
+    {
+        public AgeValidator(int age)
+        {
+            Age = age;
+        }
+
+        public int Age { get; }
+
+        public string GetErrorMessage() =>
+            $"User must be aged of {Age} years old.";
+
+        protected override ValidationResult IsValid(object value,
+            ValidationContext validationContext)
+        {
+            var userAge = ((DateTime)value);
+
+            if (!CheckUserIsOverRequiredAge(Age, userAge))
+            {
+                return new ValidationResult(GetErrorMessage());
+            }
+
+            return ValidationResult.Success;
+        }
+
+        private bool CheckUserIsOverRequiredAge(int requiredAge, DateTime dob)
+        {
+            int age;
+            age = DateTime.Now.Year - dob.Year;
+
+            age -= Convert.ToInt32(DateTime.Now.Date < dob.Date.AddYears(age));
+
+            return age >= requiredAge;
+        }
+    }
+}
